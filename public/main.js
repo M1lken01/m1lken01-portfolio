f6e527444080a9fc9fb0c78e555214927e8cc1ed
M1lken01 <xmilken01x@gmail.com>
2023-02-13 22:52:20 +0100

    tired

--- public/main.js
+++ public/main.js
@@ -1,11 +1,13 @@
-$(document).ready(function () {
-  $('a[href^="#"]').on('click', function (event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-        scrollTop: target.offset().top - 100
-      }, 600);
-    }
-  });
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 100
+            }, 600);
+        }
+    });
 });
+
+var myAge = (new Date() - new Date("2006-06-16")) / (1000 * 60 * 60 * 24 * 365.25);
